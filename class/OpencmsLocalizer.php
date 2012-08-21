a38cb42147c4f3e4ba0c11ec2bf894d48873f462
lexdene <elephant_liu@mail.dlut.edu.cn>
2012-08-21 18:20:51 +0800

    map setting by mount

--- class/OpencmsLocalizer.php
+++ class/OpencmsLocalizer.php
@@ -40,22 +40,10 @@ class OpencmsLocalizer extends Extension
 	}
 
 	public function active()
-	{	
+	{
 		$aLocale = Locale::singleton();
-		$sPrefix = DB::singleton()->tableNamePrefix();
-		$sSQLArticle = "show tables like"."\"".$sPrefix."opencms_article".'_'.str_replace('-', '_', $aLocale->localeName())."\"";
-		$sSQLAttachment = "show tables like"."\"".$sPrefix."opencms_attachment".'_'.str_replace('-', '_', $aLocale->localeName())."\"";
-		$sSQLCategory = "show tables like"."\"".$sPrefix."opencms_category".'_'.str_replace('-', '_', $aLocale->localeName())."\"";
-		
-		$aRecordsArticle = DB::singleton()->query($sSQLArticle);
-		$aRecordsAttachment = DB::singleton()->query($sSQLAttachment);
-		$aRecordsCategory = DB::singleton()->query($sSQLCategory);
-		
-		//数据表映射
-		NameMapper::singleton()->mapTableName($sPrefix.'opencms_article',$sPrefix.'opencms_article'.'_'.str_replace('-', '_', $aLocale->localeName()));
-		NameMapper::singleton()->mapTableName($sPrefix.'opencms_attachment',$sPrefix.'opencms_attachment'.'_'.str_replace('-', '_', $aLocale->localeName()));
-		NameMapper::singleton()->mapTableName($sPrefix.'opencms_category',$sPrefix.'opencms_category'.'_'.str_replace('-', '_', $aLocale->localeName()));
-		
+		$this->mapDbTable( $aLocale );
+		$this->mountSetting( $aLocale );
 	}
 	
 	//注册
@@ -77,7 +65,6 @@ class OpencmsLocalizer extends Extension
 	static public function swichLangdd($sLangCountryNew,$sLangCountryOld,$sPageUrl)
 	{
 		OpencmsLocalizer::createTable($sLangCountryNew);
-		OpencmsLocalizer::switchSetting($sLangCountryNew,$sLangCountryOld,$sPageUrl);
 	}
 	
 	//选择默认语言
@@ -142,64 +129,38 @@ class OpencmsLocalizer extends Extension
 		}
 	}
 	
-	static private function switchSetting($sLangCountryNew,$sLangCountryOld,$sPageUrl){
-		// export old
-		$aSetting = Extension::flyweight('opencms')->setting();
+	private function mapDbTable( Locale $aLocale ){
+		$sPrefix = DB::singleton()->tableNamePrefix();
+		$sSQLArticle = "show tables like"."\"".$sPrefix."opencms_article".'_'.str_replace('-', '_', $aLocale->localeName())."\"";
+		$sSQLAttachment = "show tables like"."\"".$sPrefix."opencms_attachment".'_'.str_replace('-', '_', $aLocale->localeName())."\"";
+		$sSQLCategory = "show tables like"."\"".$sPrefix."opencms_category".'_'.str_replace('-', '_', $aLocale->localeName())."\"";
+		
+		$aRecordsArticle = DB::singleton()->query($sSQLArticle);
+		$aRecordsAttachment = DB::singleton()->query($sSQLAttachment);
+		$aRecordsCategory = DB::singleton()->query($sSQLCategory);
+		
+		//数据表映射
+		NameMapper::singleton()->mapTableName($sPrefix.'opencms_article',$sPrefix.'opencms_article'.'_'.str_replace('-', '_', $aLocale->localeName()));
+		NameMapper::singleton()->mapTableName($sPrefix.'opencms_attachment',$sPrefix.'opencms_attachment'.'_'.str_replace('-', '_', $aLocale->localeName()));
+		NameMapper::singleton()->mapTableName($sPrefix.'opencms_category',$sPrefix.'opencms_category'.'_'.str_replace('-', '_', $aLocale->localeName()));
+	}
+	
+	private function mountSetting( Locale $aLocale ){
+		$aExtOpencms = Extension::flyweight('opencms');
+		$aExtOpencmsLocalizer = Extension::flyweight('opencms-localizer');
+		
+		$aSettingOpencms = $aExtOpencms->setting();
+		$aSettingOpencmsLocalizer = $aExtOpencmsLocalizer->setting();
 		
 		$arrExportKeys = array('menu','index');
-		$arrSettingExport = array();
+		
 		foreach($arrExportKeys as $sExportKey){
-			$arrSettingExport[ 'k:'.$sExportKey ] = self::exportSetting($aSetting,'/'.$sExportKey.'/');
-		}
-		
-		// save old
-		$aOlSetting = Extension::flyweight('opencms-localizer')->setting();
-		$aOlSetting->setItem(
-			'/switchSetting',
-			$sLangCountryOld,
-			'return '.var_export($arrSettingExport,true).';'
-		);
-		
-		// remove old
-		foreach($arrExportKeys as $sExportKey){
-			$aSetting->deleteKey('/'.$sExportKey,'/');
-		}
-		
-		// read new
-		$arrSettingImport = eval($aOlSetting->item(
-			'/switchSetting',
-			$sLangCountryNew,
-			'return array();'
-		));
-		
-		// import new
-		self::importSetting($aSetting,$arrSettingImport,'/');
-	}
-	
-	static private function exportSetting(Setting $aSetting,$sPath){
-		$arrRtn = array() ;
-		foreach($aSetting->keyIterator($sPath) as $aKey){
-			$arrRtn[ 'k:'.$aKey->name() ] = self::exportSetting($aSetting,$sPath.$aKey->name().'/');
-		}
-		foreach($aSetting->itemIterator($sPath) as $sItem){
-			$arrRtn[ $sItem ] = $aSetting->item($sPath,$sItem,null);
-		}
-		return $arrRtn ;
-	}
-	
-	static private function importSetting(Setting $aSetting,array $arrSettingImport , $sPath){
-		foreach($arrSettingImport as $sKey => $aValue){
-			$sPrefix = substr($sKey,0,2);
-			$sOriKey = substr($sKey,2);
-			if( 'k:' === $sPrefix ){
-				self::importSetting($aSetting,$aValue,$sPath.$sOriKey.'/');
-			}else{
-				$aSetting->setItem(
-					$sPath,
-					$sKey,
-					$aValue
-				);
-			}
+			$aSettingOpencms->mount(
+				$aSettingOpencmsLocalizer->separate(
+					$aLocale->localeName().'/'.$sExportKey
+				),
+				'/'.$sExportKey
+			);
 		}
 	}
 }
